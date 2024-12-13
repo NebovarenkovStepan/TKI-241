49042c177cc6463f73262825b7d43d28d1d0debb
Nebovarenkov Stepan <nebovarenkovstipan@gmail.com>
2024-12-13 18:26:05 +0300

    Странно, но рабоатет

--- First_task_third_semestr/Film_Tests/PersonTests.cpp
+++ First_task_third_semestr/Film_Tests/PersonTests.cpp
@@ -1,5 +1,5 @@
 #include "pch.h"
-/*#include "CppUnitTest.h"
+#include "CppUnitTest.h"
 #include "../Film/Person.h"
 #include "../Film/Movie.h"
 #include "../Film/Genre.h"
@@ -20,10 +20,11 @@ namespace FilmTests
             std::optional<std::string> patronymic = "Edward";
 
             // Act
-            auto person = Movie::Person::create_person(name, surname, patronymic);
+            Movie::Person person(name, surname, patronymic);
 
+            std::string a = "Christopher Nolan Edward";
             // Assert
-            Assert::AreEqual("Christopher Nolan Edward", person->to_string().c_str());
+            Assert::AreEqual(a, person.to_string());
         }
 
         TEST_METHOD(CreatePerson_WithoutPatronymic_Success)
@@ -34,23 +35,24 @@ namespace FilmTests
             std::optional<std::string> patronymic = std::nullopt;
 
             // Act
-            auto person = Movie::Person::create_person(name, surname, patronymic);
+            Movie::Person person(name, surname, patronymic);
 
+            std::string a = "Leonardo DiCaprio";
             // Assert
-            Assert::AreEqual("Leonardo DiCaprio", person->to_string().c_str());
+            Assert::AreEqual(a, person.to_string());
         }
 
         TEST_METHOD(AddFilmAsDirector_Success)
         {
             // Arrange
-            auto person = Movie::Person::create_person("Christopher", "Nolan");
+            Movie::Person person("Christopher", "Nolan");
             auto genre = Movie::Genre::create_genre("Action");
             std::vector<std::shared_ptr<Movie::Genre>> genres = { genre };
-            std::vector<std::shared_ptr<Movie::Person>> directors = { person };
+            std::vector<std::shared_ptr<Movie::Person>> directors = { std::make_shared<Movie::Person>(person) };
             auto movie = Movie::Movie::create_movie("Inception", 14.99, genres, directors, {});
 
             // Act
-            person->add_film_directors(movie);
+            person.add_film_directors(movie);
 
             // Assert
             Assert::IsTrue(movie->get_directors().size() == 1);
@@ -60,18 +62,18 @@ namespace FilmTests
         TEST_METHOD(AddFilmAsActor_Success)
         {
             // Arrange
-            auto person = Movie::Person::create_person("Leonardo", "DiCaprio");
+            Movie::Person person("Leonardo", "DiCaprio");
             auto genre = Movie::Genre::create_genre("Drama");
             std::vector<std::shared_ptr<Movie::Genre>> genres = { genre };
             std::vector<std::shared_ptr<Movie::Person>> directors;
             auto movie = Movie::Movie::create_movie("Titanic", 19.99, genres, directors, {});
 
             // Act
-            person->add_film_actors(movie);
+            person.add_film_actors(movie);
 
             // Assert
             Assert::IsTrue(movie->get_actors().size() == 1);
             Assert::AreEqual("Leonardo DiCaprio", movie->get_actors()[0]->to_string().c_str());
         }
     };
-}*/
+}
